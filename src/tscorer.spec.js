0cc633c4659ffcbc126d1f1d42c596bcaa0f7a2b
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-08-31 17:29:38 -0400

    Feat: test en rojo para jugador 1 anota

--- src/tscorer.spec.js
+++ src/tscorer.spec.js
@@ -1,7 +1,11 @@
 import tscorer from "./tscorer.js";
 
 describe("Tennis Scorer:", () => {
-  it("deberia mostrar marcador Love - Love", () => {
+  it("deberia mostrar el marcador en Love - Love", () => {
     expect(tscorer()).toEqual("Love - Love");
+  });
+
+  it("jugador 1 anota, deberia mostrar el marcador en 15 - Love", () => {
+    expect(tscorer()).toEqual("15 - Love");
   });
 });
