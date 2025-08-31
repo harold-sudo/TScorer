d7231bbb69c47b8e2045b6463a43f55bb2ae596d
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-08-31 19:34:51 -0400

    Feat: refactorizando código

--- src/tscorer.js
+++ src/tscorer.js
@@ -1,20 +1,38 @@
-function tscorer(fst, snd) {
-    if (fst === 1 && snd === 0) 
-    {
-        return "15 - Love";
+class TScorer {
+    constructor() {
+        this.player1Points = 0;
+        this.player2Points = 0;
     }
-    else if (fst === 1 && snd === 1)
-    {
-        return "15 - 15";
+    showScore() {
+        const scoreNames = ["Love", "15", "30", "40"];
+        return `${scoreNames[this.player1Points]} - ${scoreNames[this.player2Points]}`;
     }
-    else if (fst === 2 && snd === 1)
-    {
-        return "30 - 15";
+    player1Scores() {
+        this.player1Points++;
     }
-    else if (fst === 2 && snd === 2)
-    {
-        return "30 - 30";
+    player2Scores() {
+        this.player2Points++;
     }
-    return "Love - Love";
 }
-export default tscorer;
+
+export default TScorer;
+// function tscorer(fst, snd) {
+//     if (fst === 1 && snd === 0) 
+//     {
+//         return "15 - Love";
+//     }
+//     else if (fst === 1 && snd === 1)
+//     {
+//         return "15 - 15";
+//     }
+//     else if (fst === 2 && snd === 1)
+//     {
+//         return "30 - 15";
+//     }
+//     else if (fst === 2 && snd === 2)
+//     {
+//         return "30 - 30";
+//     }
+//     return "Love - Love";
+// }
+// export default tscorer;
--- src/tscorer.spec.js
+++ src/tscorer.spec.js
@@ -1,23 +1,43 @@
-import tscorer from "./tscorer.js";
+import TScorer from "./tscorer.js";
 
 describe("Tennis Scorer:", () => {
   it("deberia mostrar el marcador en Love - Love", () => {
-    expect(tscorer(0, 0)).toEqual("Love - Love");
+    const scorer = new TScorer();
+    expect(scorer.showScore()).toEqual("Love - Love");
+    //expect(tscorer(0, 0)).toEqual("Love - Love");
   });
 
   it("jugador 1 anota, deberia mostrar el marcador en 15 - Love", () => {
-    expect(tscorer(1, 0)).toEqual("15 - Love");
+    const scorer = new TScorer();
+    scorer.player1Scores();
+    expect(scorer.showScore()).toEqual("15 - Love");
+    //expect(tscorer(1, 0)).toEqual("15 - Love");
   });
 
   it("jugador 2 anota, genera empate, deberia mostrar el marcador en 15 - 15", () => {
-    expect(tscorer(1, 1)).toEqual("15 - 15");
+    const scorer = new TScorer();
+    scorer.player1Scores();
+    scorer.player2Scores();
+    expect(scorer.showScore()).toEqual("15 - 15");
+    //expect(tscorer(1, 1)).toEqual("15 - 15");
   });
 
   it("jugador 1 anota, deberia mostrar el marcador en 30 - 15", () => {
-    expect(tscorer(2, 1)).toEqual("30 - 15");
+    const scorer = new TScorer();
+    scorer.player1Scores();
+    scorer.player1Scores();
+    scorer.player2Scores();
+    expect(scorer.showScore()).toEqual("30 - 15");
+    //expect(tscorer(2, 1)).toEqual("30 - 15");
   });
 
   it("jugador 2 anota, genera empate, deberia mostrar el marcador en 30 - 30", () => {
-    expect(tscorer(2, 2)).toEqual("30 - 30");
+    const scorer = new TScorer();
+    scorer.player1Scores();
+    scorer.player1Scores();
+    scorer.player2Scores();
+    scorer.player2Scores();
+    expect(scorer.showScore()).toEqual("30 - 30");
+    //expect(tscorer(2, 2)).toEqual("30 - 30");
   });
 });
